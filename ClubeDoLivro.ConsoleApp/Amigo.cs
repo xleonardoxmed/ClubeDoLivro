ad0df451df5e4b850b3c68a1265744b5873caa84
leonardo medeiros <leonarodmed@hotmail.com>
2025-04-11 14:49:36 -0300

    Add project files.

---
+++ ClubeDoLivro.ConsoleApp/Amigo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDoLivro.ConsoleApp
+{
+    public class Amigo
+    {
+        public string NomeCompleto;
+        public string NomeResponsavel;
+        public string Telefone;
+
+        public Amigo(string nomeCompleto, string nomeResponsavel, string telefone)
+        {
+            NomeCompleto = nomeCompleto;
+            NomeResponsavel= nomeResponsavel;
+            Telefone = telefone;
+        }
+
+
+    }
+}
---
+++ ClubeDoLivro.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ClubeDoLivro.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TelaAmigo telaAmigo = new TelaAmigo();
+            
+            telaAmigo.InserirAmigo();
+            //telaAmigo.EditarAmigo();
+            //telaAmigo.ExcluirAmigo();
+            telaAmigo.VisualizarAmigos();
+
+            Console.ReadKey();
+        }
+    }
+}
